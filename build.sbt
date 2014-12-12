4157d55e13156abf5d56780d358c1f02cdf294c2
Mike Trupkin <mike.trupkin@gmail.com>
2014-12-11 21:39:16 -0800

    testing console-fx

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := """console-fx"""
 
 version := "1.0"
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.4"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.1.6" % "test"
 
--- src/main/scala/consolefx/ConsoleApp.scala
+++ src/main/scala/consolefx/ConsoleApp.scala
@@ -5,19 +5,54 @@ import javafx.event.ActionEvent
 import javafx.event.EventHandler
 import javafx.scene.Scene
 import javafx.scene.control.Button
-import javafx.scene.layout.StackPane
+import javafx.scene.layout._
+import javafx.scene.paint.Color
+import javafx.scene.text.{Text, Font}
 import javafx.stage.Stage
+
 import javafx.scene.control.Label
+import javax.swing.BorderFactory
+import scala.collection.JavaConversions._
 
 class ConsoleApp extends Application {
   override def start(primaryStage: Stage) {
     primaryStage.setTitle("Console FX")
 
-    val root = new StackPane
-    root.getChildren.add(new Label("Hello world!"))
+    val root = new FlowPane
+
+
+
+    val b = new Border(new BorderStroke(Color.WHITE, BorderStrokeStyle.SOLID, null, null))
+    val b2 = new Border(new BorderStroke(Color.BLUE, BorderStrokeStyle.SOLID, null, null))
+    val DOUBLE_HLINE: Char = 0x2550
+    val DOUBLE_VLINE: Char = 0x2551
+    //for( c <- "ab" + DOUBLE_HLINE + DOUBLE_HLINE + DOUBLE_VLINE + DOUBLE_VLINE + DOUBLE_VLINE + DOUBLE_VLINE + DOUBLE_VLINE + DOUBLE_VLINE + DOUBLE_VLINE + DOUBLE_VLINE) {
+    for( c <- 1 until 1000) {
+      //
+      // val l = new Label(c.toString)
+      //val l = new Text(c.toString)
+      val l2 = new Label(DOUBLE_VLINE.toString)
+
+      //l.setFont(Font.font("Consolas", 20))
+      l2.setFont(Font.font("Consolas", 20))
+      //l2.setFont(Font.font("Monospaced", 20))
+
+      //l.setBorder(b)
+      //l2.setBorder(b2)
+
+
+      //root.getChildren.add(l)
+      root.getChildren.add(l2)
+    }
 
     primaryStage.setScene(new Scene(root, 300, 300))
     primaryStage.show()
+
+
+//    println(root.getBoundsInLocal)
+//    println(root.getBoundsInParent)
+    root.getChildren.toList.foreach(n => println(n.getBoundsInParent))
+
   }
 }
 

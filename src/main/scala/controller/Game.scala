c988294998b8b722d93bc1261398f66ce60ec5e7
Mike Trupkin <mtrupkin@s4isystems.com>
2014-12-29 16:51:58 -0800

    update table content

--- src/main/scala/controller/Game.scala
+++ src/main/scala/controller/Game.scala
@@ -1,27 +1,30 @@
 package me.mtrupkin.controller
 
 
+
 import javafx.event.EventHandler
 import javafx.fxml.FXML
+import javafx.scene.control.{TableView, TableColumn, Label}
 import javafx.scene.input.KeyEvent
 import javafx.scene.layout.Pane
-import javafx.scene.control.Label
-
-
+import javafx.collections.FXCollections._
 
 import consolefx.ConsoleFx
 import me.mtrupkin.console._
-import me.mtrupkin.game.model.{Entity, Agent, World}
+import me.mtrupkin.game.model.{Agent, World}
 
+import scala.collection.JavaConversions._
 import scalafx.beans.property.StringProperty
-import scalafx.collections.ObservableBuffer
-import scalafx.scene.control.{TableColumn, TableView}
+import scalafx.scene.{control => sfxc}
 
-
+object Misc {
+  implicit def intToString(int: Int): String = int.toString
+}
+import Misc._
 
 class AgentBean(agent: Agent) {
   val name = new StringProperty(this, "name", agent.name)
-  val hp = new StringProperty(this, "hp", agent.hp.toString)
+  val hp = new StringProperty(this, "hp", agent.hp)
 }
 
 /**
@@ -31,9 +34,9 @@ trait Game { self: Controller =>
   class GameController(val world: World) extends ControllerState {
     val name = "Game"
 
-//    @FXML var encounterTable:  javafx.scene.control.TableView[AgentBean] = _
-//    @FXML var hpCol:  javafx.scene.control.TableColumn[AgentBean, Integer] = _
-//    @FXML var nameCol:  javafx.scene.control.TableColumn[AgentBean, String] = _
+    @FXML var agentTbl: TableView[AgentBean] = _
+    @FXML var nameCol:  TableColumn[AgentBean, String] = _
+    @FXML var hpCol:  TableColumn[AgentBean, String] = _
     @FXML var strText: Label = _
     @FXML var dexText: Label = _
     @FXML var intText: Label = _
@@ -52,65 +55,44 @@ trait Game { self: Controller =>
       pane.getChildren.clear()
       pane.getChildren.add(console)
       pane.setFocusTraversable(true)
+      pane.setOnKeyPressed(keyHandler)
 
-      pane.setOnKeyReleased(new EventHandler[KeyEvent] {
-        override def handle(event: KeyEvent): Unit = {
-          import Key._
-          val key = keyCodeToConsoleKey(event)
-          key match {
-            case ConsoleKey(Q, Modifiers.Control) => {
-              changeState(new IntroController)
-            }
-            case ConsoleKey(X, Modifiers.Control) => {
-              //closed = true
-            }
-            case ConsoleKey(k, _) => k match {
-              case W | Up => world.act(Points.Up)
-              case A | Left => world.act(Points.Left)
-              case S | Down => world.act(Points.Down)
-              case D | Right => world.act(Points.Right)
-              case Enter => ???
-              //case Escape => flipState(new EscapeMenuController(world))
-              case _ =>
-            }
-            case _ =>
-          }
-        }
-      })
+      agentTbl.setPlaceholder(new Label)
+      agentTbl.setMouseTransparent(true)
 
-      //val sEncounterTable = new scalafx.scene.control.TableView(encounterTable)
-//      val list = FXCollections.observableArrayList(seqAsJavaList(world.encounter.activeAgents.map(a => new AgentBean(a.name, a.hp))))
-//      nameCol.setCellValueFactory(new PropertyValueFactory[AgentBean, String]("name"))
-//      val nameCol2 = new control.TableColumn[AgentBean, String](nameCol)
-      val beans: Seq[AgentBean] = world.encounter.activeAgents.map(a => new AgentBean(a))
-//      agentModel = scalafx.collections.ObservableBuffer[AgentBean](beans)
-      import scalafx.scene.control.TableColumn._
-      agentModel ++= beans
-      content = new TableView[AgentBean](agentModel) {
-        columns ++= List(
-          new TableColumn[AgentBean, String] {
-            text = "First Name"
-            cellValueFactory = {_.value.name}
-            prefWidth = 180
-          },
-          new TableColumn[AgentBean, String]() {
-            text = "Last Name"
-            cellValueFactory = {  _.value.hp}
-            prefWidth = 180
-          }
-        )
-      }
+      nameCol.prefWidthProperty().bind(agentTbl.widthProperty().multiply(0.75))
+      hpCol.prefWidthProperty().bind(agentTbl.widthProperty().multiply(0.20))
 
-      tablePane.getChildren.clear()
-      tablePane.getChildren.add(content)
-      tablePane.setFocusTraversable(false)
-      content.focusTraversable = false
+      new sfxc.TableColumn(nameCol).cellValueFactory = { _.value.name }
+      new sfxc.TableColumn(hpCol).cellValueFactory = { _.value.hp }
 
       timer.start()
     }
 
-    val agentModel: ObservableBuffer[AgentBean] = scalafx.collections.ObservableBuffer[AgentBean]()
-    var content: TableView[AgentBean] = _
+    val keyHandler = new EventHandler[KeyEvent] {
+      override def handle(event: KeyEvent): Unit = {
+        import Key._
+        val key = keyCodeToConsoleKey(event)
+        key match {
+          case ConsoleKey(Q, Modifiers.Control) => {
+            changeState(new IntroController)
+          }
+          case ConsoleKey(X, Modifiers.Control) => {
+            //closed = true
+          }
+          case ConsoleKey(k, _) => k match {
+            case W | Up => world.act(Points.Up)
+            case A | Left => world.act(Points.Left)
+            case S | Down => world.act(Points.Down)
+            case D | Right => world.act(Points.Right)
+            case Enter => ???
+            //case Escape => flipState(new EscapeMenuController(world))
+            case _ =>
+          }
+          case _ =>
+        }
+      }
+    }
 
     def keyCodeToConsoleKey(event: KeyEvent): ConsoleKey = {
       val modifier = Modifier(event.isShiftDown, event.isControlDown, event.isAltDown)
@@ -120,20 +102,17 @@ trait Game { self: Controller =>
     }
 
     def update(elapsed: Int): Unit = {
-      implicit def toString(int: Int): String = int.toString
       import world.player._
+
+      val agentModel = observableArrayList[AgentBean](world.encounter.activeAgents.map(a => new AgentBean(a)))
+      agentTbl.setItems(agentModel)
 
       strText.setText(stats.str)
       dexText.setText(stats.dex)
       intText.setText(stats.int)
       hpText.setText(hp)
 
-
-      val beans: Seq[AgentBean] = world.encounter.activeAgents.map(a => new AgentBean(a))
-//      agentModel.filter()
-      content.items = scalafx.collections.ObservableBuffer[AgentBean](beans)
       world.render(screen)
-
       console.draw(screen)
     }
   }
